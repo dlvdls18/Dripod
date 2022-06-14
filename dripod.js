3485c6c7f2c1179c806ef2821559d6ce1242dadf
Delve Delos Santos Jr <106716453+dlvdls18@users.noreply.github.com>
2022-06-15 07:39:37 +0800

    Add files via upload

---
+++ dripod.js
@@ -0,0 +1,44 @@
+function Dripod(text, config) {
+  var bytes = [];
+  var res = [];
+  Array.from(text).forEach(function(char) {
+    bytes.push(char.charCodeAt(0).toString(2));
+  })
+  if(!config) return bytes;
+  var byteIndex = 0;
+  var newBytes = [];
+  bytes.forEach(function(byte) {
+    if(config["ins_" + byteIndex] != null) {
+      newBytes.push(config["ins_" + byteIndex]);
+      byteIndex++;
+    }
+    newBytes.push(byte);
+    byteIndex++;
+  });
+  if(config["ins_" + byteIndex] != null) newBytes.push(config["ins_" + byteIndex]);
+  bytes = newBytes;
+  byteIndex = 0;
+  var bitIndex = 0;
+  bytes.forEach(function(byte) {
+    var whole = "";
+    if(config["mod_" + byteIndex] != null) byte = config["mod_" + byteIndex];
+    if(config["dest_" + byteIndex] != null) {
+      byteIndex++;
+      return;
+    }
+    Array.from(byte).forEach(function(bit) {
+      if(config["mod_" + byteIndex + "_at_" + bitIndex] != null) bit = config["mod_" + byteIndex + "_at_" + bitIndex];
+      if(config["dest_" + byteIndex + "_at_" + bitIndex] != null) {
+        bitIndex++;
+        return;
+      }
+      whole += bit;
+      bitIndex++;
+    });
+    res.push(whole);
+    whole = "";
+    bitIndex = 0;
+    byteIndex++;
+  });
+  return res;
+}

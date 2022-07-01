74f1a2bb67d2a1cd412ad2cd9a1742424f85923b
Delve Delos Santos Jr <106716453+dlvdls18@users.noreply.github.com>
2022-07-01 11:20:43 +0800

    update dripod.js

--- src/dripod.js
+++ src/dripod.js
@@ -45,7 +45,7 @@ function Dripod(text, config) {
 Dripod.Builder = function(bytes) {
   var str = "";
   bytes.forEach(function(byte) {
-    
+    str += String.fromCharCode()
   });
   return str;"
 }
